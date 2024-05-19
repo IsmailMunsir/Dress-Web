a39ad7cf90a14d27b04a12d03b4c78aec2d53593
ahamed <ahamedismail12345@gmail.com>
2024-05-19 07:19:27 +0530

    first commit

---
+++ Java.js
@@ -0,0 +1,13 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 0 );
+})
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('#navmenu');
+
+
+menu.onclick= () => {
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open');
+}
